e47d6654b97be336ffc34c9c49d0d3187ada65c9
Alvaro Cabrera <pateketrueke@gmail.com>
2023-01-25 03:28:41 -0600

    fix: relax regex for tag-selectors

--- src/lib/shared.js
+++ src/lib/shared.js
@@ -5,7 +5,7 @@ export const RE_XML_CLOSE_BEGIN = /^<\/\w/;
 export const SVG_NS = 'http://www.w3.org/2000/svg';
 
 export const XLINK_NS = 'http://www.w3.org/1999/xlink';
-export const ELEM_REGEX = /^(\w*|[.#]\w+)(#[\w-]+)?(\.[\w-][\w-.]*)*$/;
+export const ELEM_REGEX = /^([\w-]*|[.#]\w+)(#[\w-]+)?(\.[\w-][\w-.]*)*$/;
 
 export const EE_SUPPORTED = ['oncreate', 'onupdate', 'ondestroy'];
 
